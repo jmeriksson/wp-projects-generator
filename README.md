3dcd1a3d7c7a873a267f642fce78a217629c9fa3
Mikael Eriksson <mikael.eriksson.nkpg@gmail.com>
2020-05-23 21:34:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Projects Generator
+
+This is a WordPress plugin that creates a custom post type 'project' in which the user
+can create courses or educational programs which will automatically get added to their
+WordPress website with a pre-defined landing page design.
---
+++ projects-generator.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package ProjectsGenerator
+ */
+
+/*
+ Plugin Name:     Projects Generatpr
+ Plugin URI:      https://github.com/jmeriksson/wp-projects-generator.git
+ Description:     A simple WordPress plugin that generates a 'project' custom post type with predefined styling.
+ Version:         1.0.0
+ Author:          Mikael Eriksson
+ Author URI:      https://mikaeleriksson.nu
+ License:         GPL-3.0+
+ LicenseURI:      https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+ // Prevents public access to the plugin via URL  
+if ( ! defined( 'ABSPATH' ) ) {
+  exit;
+}
